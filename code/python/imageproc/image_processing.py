6bbc343616e29af51ad86c362641c031d293ff7e
Abby Lijiao Huang <lh8xj@virginia.edu>
2015-11-28 22:24:48 -0500

    add homework imageproc

---
+++ code/python/imageproc/image_processing.py
@@ -0,0 +1,125 @@
+import pygame as pg
+
+################### PIXEL OPERATIONS ######################
+
+# grayPixel: pixel -> pixel
+# compute and return a gray pixel with the same intensity
+# as the given pixel. A little complication: the intensity
+# values in the 3D numpy array representing the pixels are
+# expressed as 8-bit integers. The range is 0..255. The
+# problem is that if one adds together three of these, to
+# get aonther 8-bit integer, there can be an overflow! So
+# we convert the 8-bit integers to values of type "int" before
+# we do the addition. The "int" type in Python can hold
+# integer values of any size.
+#
+def grayPixel(pixel):
+    red_intensity = int(pixel[0])
+    green_intensity = int(pixel[1])
+    blue_intensity = int(pixel[2])
+    ave_intensity = (red_intensity + green_intensity + blue_intensity)//3
+    
+    return ((ave_intensity, ave_intensity, ave_intensity))
+
+# channel: pixel -> channel -> pixel
+# return a gray pixel with intensity from given channel of given pixel
+def channel(pixel,chan):
+    return (pixel[chan],pixel[chan],pixel[chan])
+
+
+# inverse: pixel -> pixel
+# return the color negative of the given pixel
+def inverse(pixel):
+    return (255-pixel[0], 255-pixel[1], 255-pixel[2])
+
+
+# intensify: pixel -> nat255 -> pixel
+# brighten each channel of pixel by quantity
+#
+# NOTE: there might be an overflow bug in this code!
+# If so, reason through it and fix it. Consider the
+# possible need for a function that brightens an
+# individual pixel intensity, never returning a result
+# greater than 255 or less than 0.
+#
+def pixellimit (value,quantity):
+    if (value + quantity > 255):
+       new = 255
+    elif (value + quantity < 0):
+       new = 0
+    else:
+        new = value + quantity 
+    return new 
+    
+def intensify(pixel,quantity):
+    new0 = pixellimit (pixel[0], quantity)
+    new1 = pixellimit (pixel[1],quantity)
+    new2 = pixellimit (pixel[2], quantity)
+    return (new0, new1, new2)
+
+
+################### IMAGE OPERATIONS ######################
+
+# invert: modifies image pixel array of image_surf in place
+# replace each pixel with its photographic "negative"
+#
+def invert(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = inverse(pixels3d[x,y])
+
+#Brighten the image
+def brighten (image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = intensify(pixels3d[x,y],20)
+
+def darken (image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = intensify(pixels3d[x,y],-20)
+
+
+
+
+# bw: modifies image pixel array of image_surf in place
+# replaces each pixel with a corresponding gray-scale pixel
+def bw(image_surf):    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = grayPixel(pixels3d[x,y])
+
